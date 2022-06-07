56a0037bdef7bdcc13afef3e12df049ae0dba02e
Adventistforever <43876940+Adventistforever@users.noreply.github.com>
2022-06-07 09:39:17 +0200

    SETUP

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ variable just leave it set to None or an empty list
 """
 
 #  can be a member ID, channel ID
-ERROR_SEND_TARGET = None
+ERROR_SEND_TARGET = 960109774370910238
 
 # should be a comma separated list of role IDs you
 # wish to monitor for custom DM messages
@@ -12,10 +12,10 @@ ROLES_FOR_DM = []
 
 # should be a comma separated list of role IDs you
 # wish to monitor for sending channel messages
-ROLE_FOR_CHANNEL_MSG = []
+ROLE_FOR_CHANNEL_MSG = [960123505511383130]
 
 # the ID for the channel you wish the ROLE_FOR_CHANNEL_MSG to be sent to.
-SEND_CHANNEL = None
+SEND_CHANNEL = 960103626674675772
 
 # the ID for the role you wish to auto remove when one of the
 # roles in ROLE_FOR_DM is assigned to the member
